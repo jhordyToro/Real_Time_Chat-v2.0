a40cc6fcddddc794dc8395f2fb0696c8b70fe502
Jhordy Toro <jhordytoro49@gmail.com>
2022-06-15 20:43:57 -0500

    all it's ready

--- djangochat/room/views.py
+++ djangochat/room/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Room
+from .models import Room, Message
 
 # Create your views here.
 
@@ -13,5 +13,6 @@ def rooms(request):
 @login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:25]
 
-    return render(request, 'room/room.html', {'room': room})
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
