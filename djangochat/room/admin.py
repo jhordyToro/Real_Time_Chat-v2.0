4eb9a8284a5bb50dacd23e4840e83cd35a8a1d3e
Jhordy Toro <jhordytoro49@gmail.com>
2022-06-24 22:00:58 -0500

    update

--- djangochat/room/admin.py
+++ djangochat/room/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Room
+from .models import Room, Message
 
 # Register your models here.
 
 admin.site.register(Room)
+admin.site.register(Message)
